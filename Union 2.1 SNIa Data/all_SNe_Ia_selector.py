b724c4c7a25677b8306f8ed29666c8fb11f17d55
Unknown <alex.d.hunt@icloud.com>
2018-01-31 03:09:51 +0000

    #### Milestone Finished - Union 2.1 Data Research ####

---
+++ Union 2.1 SNIa Data/all_SNe_Ia_selector.py
@@ -0,0 +1,27 @@
+"""Picks out all type 'Ia' Supernovae from 'All SNe 1953-2015.txt' and puts \
+their: Name, Type, Right Ascension, Declination, Effective Magnitude and \
+Discoverer(s), into a new file called 'All SNe Ia.txt'. Data is from: \
+http://www.cbat.eps.harvard.edu/lists/Supernovae.html."""
+
+with open("All SNe 1953-2015.txt", 'r') as f:
+    data = f.readlines()[1:]
+
+SNe_Ia = []
+for line in data:
+    if len(line) > 5:
+        if line[130:132] == 'Ia':
+            name, SNtype = line[0:6].strip(), line[130:134].strip()
+            coords, mag, discoverer = line[87:110], line[64:68], line[144:-1]
+            if coords == ' '*23:
+                 ra, decl =  'N/A', 'N/A'
+            else:
+                ra = coords[:11].replace(' ', ':')
+                decl = coords[12:].replace(' ', ':')
+            SNe_Ia.append([name, SNtype, ra, decl, mag, discoverer])
+
+with open('All SNe Ia.txt', 'w') as f:
+    f.write('#SN_name , SN_type , R.A. , Decl. , Eff_Mag , Discoverer(s)\n')
+    for name, SNtype, ra, decl, mag, discoverer in SNe_Ia:
+        f.write('{0} , {1} , {2} , {3} , {4} , {5}\n'.format(name, SNtype, ra,
+                                                             decl, mag,
+                                                             discoverer))
---
+++ Union 2.1 SNIa Data/all_SNe_Union2.1_cutter.py
@@ -0,0 +1,22 @@
+"""Picks out suitable 'uncut' Supernovae from the Union2.1 dataset \
+'SCPUnion2.1_AllSNe_table.tex' and puts their: SN Name, Redshift, \
+Effective Magnitude, Magnitude Error and the Original Dataset, into a new file \
+called 'All SNe Union2.1.txt'. Data is from: http://supernova.lbl.gov/Union/, \
+under: 'Cosmology Tables' > 'Full Table of All SNe'.
+"""
+
+with open("SCPUnion2.1_AllSNe_table.tex", 'r') as f:
+    data = f.readlines()
+
+all_Union_uncut = []
+for line in data:
+    split_line = line[:-4].split(' & ')
+    if split_line[-1] == '\\nodata':
+        name, rdshft = split_line[:2]
+        eff_mag, err_mag = split_line[2][:-1].split('(')
+        all_Union_uncut.append([name, rdshft, eff_mag, err_mag, '----'])
+
+with open('All SNe Union2.1.txt', 'w') as f:
+    f.write('#SN_name , Redshift , eff_mag , err_mag , dataset\n')
+    for name, rdshft, eff_mag, err_mag, dataset in all_Union_uncut:
+        f.write('{0} , {1} , {2} , {3} , {4}\n'.format(name, rdshft, eff_mag, err_mag, dataset))
