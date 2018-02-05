250fce4f83528b5db61005f197fa27d4e7a8bcaf
Unknown <alex.d.hunt@icloud.com>
2018-02-05 11:21:11 +0000

    Union 2.1 data code changed to add the SNe from the 'SuperNova Data.txt' file.

--- Union 2.1 SNIa Data/all_SNe_Union21_cutter.py
+++ Union 2.1 SNIa Data/all_SNe_Union21_cutter.py
@@ -6,8 +6,18 @@ under: 'Cosmology Tables' > 'Full Table of All SNe'.
 """
 
 folder = '/media/alex/Shared/University/Physics/Year 3/Physics Problem Solving/Computing Project/Programming/Milestone-Program-Supernova/Union 2.1 SNIa Data/'
+with open(folder+"../SuperNova Data.txt", 'r') as f:
+    data = f.readlines()
+old_data, old_SNe_names, counter = [], {}, 0
+for line in data:
+    if (line[0] != '#') and (line.strip('\n') != ''):
+        line = line[:-1].split(' ')
+        old_data.append(line)
+        old_SNe_names[line[0].upper()] = counter
+        counter += 1
 with open(folder+"SCPUnion2.1_AllSNe_table.tex", 'r') as f:
     data = f.readlines()
+
 
 all_Union_uncut = []
 for line in data:
@@ -16,6 +26,13 @@ for line in data:
         name, rdshft = line[:2]
         eff_mag, err_mag = line[2][:-1].split('(')
         all_Union_uncut.append([name, rdshft, eff_mag, err_mag, '----'])
+        if name.upper() in old_SNe_names:
+            del old_SNe_names[name.upper()]
+for SN, n in old_SNe_names.items():
+    line = old_data[n]
+    line.append('----')
+    all_Union_uncut.append(line)
+
 
 with open(folder+'All SNe Union2.1.txt', 'w') as f:
     f.write('#SN_name , Redshift , eff_mag , err_mag , dataset\n')
